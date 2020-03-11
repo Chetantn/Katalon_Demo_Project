c52939b78664855849bcf1627b5f5445ab4e6e0b
tejakant <tejakant@MOB81269.hq.k.grp>
2020-03-12 00:22:01 +0530

    First Commit

---
+++ Test Suites/SOAP_SUITE.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SOAP_SUITE</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>795a404a-d509-41da-b7c1-696ecd88147a</testSuiteGuid>
+   <testCaseLink>
+      <guid>a6778c50-e0b2-4959-b863-c0357e4e47c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SOAP_TEST_CALCULATOR/Add_Two_Numbers</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>31355f99-d5fe-489a-bc91-92eef5429485</testSuiteGuid>
+   <testCaseLink>
+      <guid>aedbc797-7362-44be-a364-4f4008fe4ed5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RestTest/HTTPMETHODS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>45bc2cfe-1e6b-4915-b709-040417f592b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RestTest/Sample_Req_Res</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
